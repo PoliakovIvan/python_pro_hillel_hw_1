7a29c923bcd2fe572c9e53bf852ce15d533d4663
Ivan <ivan.polyakoff2003@gmail.com>
2022-10-17 22:34:07 +0300

    add chenges main.py(3)

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import names
 
 menu = input('menu:'
             '\n1. Full name'
-            '\n2. First name\n')
+            '\n2. First name'
+            '\n3. Last name \n')
 gender = input('chose gender: M/F')
 
 def full_name(gender):
@@ -22,16 +23,19 @@ def first_name(gender):
         first_n = names.get_first_name(gender='female')
     return first_n
 
-
+def last_name():
+    last_n = names.get_last_name()
+    return last_n
 
 full = full_name(gender)
 first = first_name(gender)
-
+last = last_name()
 
 if menu == '1':
     print(full)
 elif menu == '2':
     print(first)
-
+elif menu == '3':
+    print(last)
 else:
     print('please, print numbers 1-3')
